b3254d504c04fb61322576eb83ac32f433cab4eb
Hecatonquir <maxicuentas92@hotmail.com>
2022-09-16 19:21:01 -0300

    Added minimun CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from 'react';
+import {
+	AppShell,
+	Navbar,
+	Header,
+	Footer,
+	Aside,
+	Text,
+	MediaQuery,
+	Burger,
+	useMantineTheme,
+} from '@mantine/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const theme = useMantineTheme();
+	const [opened, setOpened] = useState(false);
+
+	//-----------------
+	//-----------------
+	return (
+		<AppShell
+			styles={{
+				main: {
+					background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+				},
+			}}
+			navbarOffsetBreakpoint='sm'
+			asideOffsetBreakpoint='sm'
+			navbar={
+				<Navbar p='md' hiddenBreakpoint='sm' hidden={!opened} width={{ sm: 200, lg: 300 }}>
+					<Text>📍Vinos</Text>
+					<Text>Cerveza</Text>
+					<Text>📍Vinos</Text>
+					<Text>📍Vinos</Text>
+					<Text>📍Vinos</Text>
+					<Text>📍Vinos</Text>
+					<Text>Application navbar</Text>
+					<Text>Application navbar</Text>
+					<Text>Application navbar</Text>
+				</Navbar>
+			}
+			aside={
+				<MediaQuery smallerThan='sm' styles={{ display: 'none' }}>
+					<Aside p='md' hiddenBreakpoint='sm' width={{ sm: 200, lg: 300 }}>
+						<Text>Application sidebar</Text>
+					</Aside>
+				</MediaQuery>
+			}
+			footer={
+				<Footer height={60} p='md'>
+					Application footer
+				</Footer>
+			}
+			header={
+				<Header height={70} p='md'>
+					<div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+						<MediaQuery largerThan='sm' styles={{ display: 'none' }}>
+							<Burger
+								opened={opened}
+								onClick={() => setOpened((o) => !o)}
+								size='sm'
+								color={theme.colors.gray[6]}
+								mr='xl'
+							/>
+						</MediaQuery>
+
+						<Text> ⚠️ Previatek</Text>
+					</div>
+				</Header>
+			}>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+			<h1>TARJETA</h1>
+		</AppShell>
+	);
 }
-
-export default App;
