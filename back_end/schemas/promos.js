a14ea0e9b582182f845f45d911267b4f6cc42fd4
Hecatonquir <maxicuentas92@hotmail.com>
2022-10-11 21:44:33 -0300

    added Backend with Sanity

---
+++ back_end/schemas/promos.js
@@ -0,0 +1,35 @@
+export default {
+	name: 'promos',
+	title: 'Promos',
+	type: 'document',
+	fields: [
+		{
+			name: 'nombre',
+			title: 'Nombre',
+			type: 'string',
+		},
+		{
+			name: 'precio',
+			title: 'Precio',
+			type: 'number',
+		},
+		{
+			name: 'disponible',
+			title: 'Disponible',
+			type: 'string',
+		},
+		{
+			name: 'categoria',
+			title: 'Categoria',
+			type: 'string',
+		},
+		{
+			name: 'imagen',
+			title: 'Imagen',
+			type: 'image',
+			options: {
+				hotspot: true,
+			},
+		},
+	],
+};
