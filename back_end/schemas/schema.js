69460d9d53d577fe24f65f7946e844e2b14cda07
Hecatonquir <maxicuentas92@hotmail.com>
2022-10-12 01:53:52 -0300

    finished creating back end

--- back_end/schemas/schema.js
+++ back_end/schemas/schema.js
@@ -1,9 +1,21 @@
 // First, we must import the schema creator
 import createSchema from 'part:@sanity/base/schema-creator';
-
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 import promos from './promos';
+import aperitivos from './aperitivos';
+import blancos from './blancos';
+import cervezas from './cervezas';
+import champagnes from './champagnes';
+import cigarrillos from './cigarrillos';
+import energizantes from './energizantes';
+import gaseosas from './gaseosas';
+import gin_ron from './gin_ron';
+import vodkas from './vodkas';
+import tintos from './tintos';
+import jugos from './jugos';
+import hielo_vasos from './hielo_vasos';
+
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
 	// We name our schema
@@ -13,5 +25,17 @@ export default createSchema({
 	types: schemaTypes.concat([
 		/* Your types here! */
 		promos,
+		aperitivos,
+		cervezas,
+		vodkas,
+		tintos,
+		blancos,
+		gaseosas,
+		champagnes,
+		gin_ron,
+		energizantes,
+		jugos,
+		cigarrillos,
+		hielo_vasos,
 	]),
 });
