a9e33de978a88370f13a457a62af661e6dfea083
FacuRamirez <framirezforsyth@gmail.com>
2022-10-19 00:00:11 -0300

    firebase+sanity

---
+++ front_end/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAuq_dEg2TPVOQMNGC2Dhh8H0d5R2G2HYk",
+  authDomain: "previatecapedidos.firebaseapp.com",
+  projectId: "previatecapedidos",
+  storageBucket: "previatecapedidos.appspot.com",
+  messagingSenderId: "987683357639",
+  appId: "1:987683357639:web:d2b75d8601de217c0a8fe2",
+
+  REACT_APP_SANITY_PROJECT_ID: "qfol1u3p",
+  REACT_APP_SANITY_TOKEN: "skjXxeakdxZ1QcWbp8fqtIgUA0jiGc9eV1Id1PPrcIGz2ADp2268vshsIgXY7qPTeIlRt4SCEe63o0qr41cWePk4NOzQTlLIIrQzHnSyEbejcKCvKcgottHfr9YozylAs9MhTzFNvv0VHTL63UiiH9AChgbuG2pj1z8TEMUscVx0p3DcrxjC"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
--- front_end/src/index.js
+++ front_end/src/index.js
@@ -5,6 +5,7 @@ import { store } from './redux/store';
 import './index.css';
 import App from './App';
 import { MantineProvider } from '@mantine/core';
+import './firebase'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
